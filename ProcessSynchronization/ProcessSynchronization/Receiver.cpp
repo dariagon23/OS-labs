cc8028bb2aab3fc7129380b2748ddf9ae9218793
Daria Gonchar <gonchardaria23@gmail.com>
2022-05-15 21:14:17 +0300

    creation of binary file

--- ProcessSynchronization/ProcessSynchronization/Receiver.cpp
+++ ProcessSynchronization/ProcessSynchronization/Receiver.cpp
@@ -6,5 +6,17 @@
 
 int main()
 {
+	char binFileName[100];
+	int count; //the numer of records
+	std::cout << "Input binary file name:" << std::endl;
+	std::cin >> binFileName;
+	std::cout << "Input the number of records in the binary file:" << std::endl;
+	std::cin >> count;
+	std::fstream file(binFileName,std::ios::binary|std::ios::out);
+	if (!file.is_open()) {
+		std::cout << "Error while creating file!" << std::endl;
+		return 0;
+	}
+	file.close();
 	return 0;
 }
